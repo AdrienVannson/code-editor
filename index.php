56f5655157ab84216ffb9bfe599119dd4474a654
Adrien VANNSON <adrien.vannson@gmail.com>
2018-02-16 21:17:01 +0100

    Ajout d'un menu

--- index.php
+++ index.php
@@ -1,1 +1,34 @@
-:P
+<?php
+$couleurTheme = 'cyan';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+<title>Code Editor</title>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link type="text/css" rel="stylesheet" href="static/materialize/css/materialize.min.css"  media="screen,projection"/>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+
+</head>
+
+<body>
+
+
+<!-- Barre de menu -->
+<nav>
+    <div class="nav-wrapper <?=$couleurTheme?>">
+        <a href="#" class="brand-logo">Code Editor</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="#">New</a></li>
+            <li><a href="#">Open</a></li>
+        </ul>
+    </div>
+</nav>
+
+<script type="text/javascript" src="static/materialize/js/materialize.min.js"></script>
+
+</body>
+</html>
