031dbbd4b5d235d319ef7dc3b69bddb5fec45fa6
Adrien VANNSON <adrien.vannson@gmail.com>
2018-02-16 21:42:27 +0100

    Ajout d'un éditeur de code

--- index.php
+++ index.php
@@ -7,8 +7,12 @@ $couleurTheme = 'cyan';
 <head>
 
 <title>Code Editor</title>
+
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <link type="text/css" rel="stylesheet" href="static/materialize/css/materialize.min.css"  media="screen,projection"/>
+
+<link rel="stylesheet" href="static/codemirror-5.34.0/lib/codemirror.css">
+<link rel="stylesheet" href="static/codemirror-5.34.0/theme/dracula.css">
 
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
@@ -28,7 +32,31 @@ $couleurTheme = 'cyan';
     </div>
 </nav>
 
+<!-- Code editor -->
+<textarea id="code-editor">
+#include <iostream>
+
+using namespace std;
+
+int main ()
+{
+    return 0;
+}
+</textarea>
+
+
 <script type="text/javascript" src="static/materialize/js/materialize.min.js"></script>
+
+<script src="static/codemirror-5.34.0/lib/codemirror.js"></script>
+<script src="static/codemirror-5.34.0/mode/clike/clike.js"></script>
+
+<script>
+    var editor = CodeMirror.fromTextArea(document.getElementById('code-editor'), {
+        lineNumbers: true,
+        theme: 'dracula',
+        mode: "text/x-c++src"
+    });
+</script>
 
 </body>
 </html>
